0ad78283f23a9ad43fd5538892f1919d77c8b3f7
lukagbn <luka68690@gmail.com>
2025-12-10 14:04:39 +0400

    updated slider animation logic

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const profileSlider = document.getElementById("aboutMe");
 const diagram = profileSlider.querySelector(".diagram");
 const diagramFill = diagram.querySelectorAll(".fill");
 const diagramCircle = diagram.querySelectorAll(".circle");
+
 function sliderAnimation(index, width, left) {
   diagramFill[index].style.width = `${width}%`;
   diagramCircle[index].style.left = `${left}%`;
 }
-// testing slider animation
-profileSlider.addEventListener("click", () => {
-  sliderAnimation(0, 72, 70);
-  sliderAnimation(1, 50, 48);
-  sliderAnimation(2, 60, 58);
-  sliderAnimation(3, 84, 82);
-});
+// animating sliders
+const options = {
+  root: null,
+  threshold: 0.3,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      sliderAnimation(0, 72, 70);
+      sliderAnimation(1, 50, 48);
+      sliderAnimation(2, 60, 58);
+      sliderAnimation(3, 84, 82);
+      observer.unobserve(entry.target);
+    }
+    return;
+  });
+}, options);
+observer.observe(profileSlider);
